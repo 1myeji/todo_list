b17f96446b18d308de6b7fe0bf4bb4ae403bf9ec
1myeji <106291546+1myeji@users.noreply.github.com>
2022-11-27 17:22:05 +0900

    수정 전

---
+++ addTodo.js
@@ -0,0 +1,17 @@
+// 항목 추가
+export async function addTodo(title) {
+  const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'apikey': 'FcKdtJs202209',
+      'username': 'KDT3_ImYeJi'
+    },
+    body: JSON.stringify({
+      title
+    })
+  })
+  const json = await res.json()
+  console.log(json)
+  return json
+}
