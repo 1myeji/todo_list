a1011596f6a2c03f54ce2a53a67025d423d4d9fd
1myeji <106291546+1myeji@users.noreply.github.com>
2022-11-29 16:32:51 +0900

    feat: show latest updated date

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-import { getTodo } from "./getTodo.js"
-import { addTodo } from "./addTodo.js"
-import { deleteTodo } from "./deleteTodo.js"
-import { editTodo } from "./editTodo.js"
-import { changeOrder } from "./changeOrder.js"
+import { getTodo, addTodo, deleteTodo, editTodo, changeOrder } from "./api.js"
 
-const alert = document.querySelector('.alert')
+const alert = document.querySelector('.todo-alert')
 const form = document.querySelector('.todo-form')
 const todo = document.querySelector('.todo')
 const submitBtn = document.querySelector('.submit-btn')
-const container = document.querySelector('.container')
+const container = document.querySelector('.todocontainer')
 const list = document.querySelector('.list')
 const clearBtn = document.querySelector('.clear-btn')
 const orderchangeBtn = document.querySelector('.orderchange-btn')
+const spinner = document.querySelector('.spinner-border')
 
 let editlist
+let editupdate
 let editFlag = false
 let overlap = false
 let editID
 let done = false // 일단 해둠
 let editOrder
 let idArray= []
+let update 
 
 // 새로고침
+window.onbeforeunload = () => {
+  spinner.classList.add('show-container')
+}
 window.addEventListener('DOMContentLoaded', loadlist)
 // submit 하면
 form.addEventListener('submit', addlist)
@@ -41,21 +43,28 @@ async function addlist(e) {
     } 
   }
   if(title && !editFlag && !overlap){
+    spinner.classList.add('show-container')
     let lists = await getTodo()
     let order = lists.length
     let todos = await addTodo(title, order)
-    const { id } = todos
+    const { id, updatedAt } = todos
+    date(updatedAt)
     idArray.push(id)
-    createTodo(title, id)
-    console.log(idArray);
+    createTodo(title, id, update)
     displayAlert('todo added to the list', 'success')
     container.classList.add('show-container')
+    spinner.classList.remove('show-container')
     setBackToDefault()
   } 
   else if(title && editFlag && !overlap){
+    spinner.classList.add('show-container')
     editlist.innerHTML = title
-    await editTodo(editID, title, done, editOrder)
+    let lists = await editTodo(editID, title, done, editOrder)
+    const { updatedAt } = lists
+    date(updatedAt)
+    editupdate.innerHTML = update
     displayAlert('value changed', 'success')
+    spinner.classList.remove('show-container')
     setBackToDefault()
   }
   else if(!title && !overlap){
@@ -64,7 +73,19 @@ async function addlist(e) {
   setBackToDefault()
 }
 
-function createTodo(title, id) {
+// 날짜 포맷 변경
+function date (updatedAt) {
+  let date = new Date(updatedAt);
+  let year = date.getFullYear();
+  let month = ('0' + (date.getMonth() + 1)).slice(-2);
+  let day = ('0' + date.getDate()).slice(-2);
+  let hours = ('0' + date.getHours()).slice(-2); 
+  let minutes = ('0' + date.getMinutes()).slice(-2);
+  let seconds = ('0' + date.getSeconds()).slice(-2);
+  update = `update : ${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
+
+function createTodo(title, id, update) {
   const element = document.createElement('article')
   // add class
   element.classList.add('item')
@@ -73,6 +94,7 @@ function createTodo(title, id) {
   element.setAttributeNode(attr)
   element.innerHTML = /* html */`
     <p class="title">${title}</p>
+    <p class="update">${update}</p>
     <div class="btn-container">
       <button type="button" class="edit-btn">
         <i class="fas fa-edit"></i>
@@ -93,16 +115,17 @@ function createTodo(title, id) {
 // display alert
 function displayAlert(text, action) {
   alert.textContent = text
-  alert.classList.add(`alert-${action}`)
+  alert.classList.add(`todoalert-${action}`)
 
   // remove alert
   setTimeout(function() {
     alert.textContent = ''
-    alert.classList.remove(`alert-${action}`)
+    alert.classList.remove(`todoalert-${action}`)
   }, 1000)
 }
 
 async function clearlist() {
+  spinner.classList.add('show-container')
   const items = document.querySelectorAll('.item')
   if(items.length > 0) {
     items.forEach(item => {
@@ -117,10 +140,12 @@ async function clearlist() {
   }
   container.classList.remove('show-container')
   displayAlert('empty list', 'danger')
+  spinner.classList.remove('show-container')
   setBackToDefault()
 }
 
 async function deleteItem(e) {
+  spinner.classList.add('show-container')
   const element = e.currentTarget.parentElement.parentElement
   const title = element.querySelector('p').textContent
   list.removeChild(element)
@@ -128,7 +153,6 @@ async function deleteItem(e) {
     container.classList.remove('show-container')
   }
   displayAlert('item removed', 'danger')
-  
   let lists = await getTodo()
   for(const list of lists) {
     if(title === list.title) {
@@ -137,6 +161,7 @@ async function deleteItem(e) {
       idArray.filter(value => value !== id)
     }
   }
+  spinner.classList.remove('show-container')
   // delete시 order 변경
   lists = await getTodo()
   for(let i = 0; i < lists.length; i++) {
@@ -150,7 +175,8 @@ async function deleteItem(e) {
 }
 
 async function editItem(e) {
-  editlist = e.currentTarget.parentElement.previousElementSibling
+  editlist = e.currentTarget.parentElement.previousElementSibling.previousElementSibling
+  editupdate = e.currentTarget.parentElement.previousElementSibling
   todo.value = editlist.innerHTML
   let lists = await getTodo()
   for(const list of lists) {
@@ -165,24 +191,20 @@ async function editItem(e) {
   submitBtn.textContent = 'edit'
 }
 
-// set back to default
-function setBackToDefault () {
-  todo.value = ''
-  editFlag = false
-  overlap = false
-  submitBtn.textContent = 'submit'
-}
-
+// 새로고침시
 async function loadlist() {
   let lists = await getTodo()
   if(lists.length > 0) {
     container.classList.add('show-container')
     for(const list of lists) {
-      createTodo(list.title, list.id)
+      date(list.updatedAt)
+      createTodo(list.title, list.id, update)
       idArray.push(list.id)
     }
   }
 }
+
+// 목록 순서 변경
 new Sortable(list, {
   animation: 150,
   ghostClass: 'blue-backgorund-class'
@@ -190,16 +212,22 @@ new Sortable(list, {
 async function changelist () {
   for(let i = 0; i < idArray.length; i++) {
     const pTitle = document.querySelectorAll('.title')[i].textContent
-    console.log(pTitle);
     let lists = await getTodo()
     if (pTitle != lists[i].title) {
       let articleArray = []
       for(let j = 0; j < idArray.length; j++) {
         let articleId = document.querySelectorAll('.item')[j].dataset.id
         articleArray.push(articleId)
-        console.log(articleArray);
         await changeOrder(articleArray)
       }
     } 
   }
+}
+
+// set back to default
+function setBackToDefault () {
+  todo.value = ''
+  editFlag = false
+  overlap = false
+  submitBtn.textContent = 'submit'
 }
