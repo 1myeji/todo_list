c89a79d0f83a2c9c6a0d11c40496d0bda0feb501
1myeji <106291546+1myeji@users.noreply.github.com>
2022-12-01 16:37:55 +0900

    feat: Create store.js

---
+++ store.js
@@ -0,0 +1,23 @@
+export const alert = document.querySelector('.todo-alert')
+export const form = document.querySelector('.todo-form')
+export const todo = document.querySelector('.todo')
+export const submitBtn = document.querySelector('.submit-btn')
+export const container = document.querySelector('.todocontainer')
+export const list = document.querySelector('.list')
+export const clearBtn = document.querySelector('.clear-btn')
+export const orderchangeBtn = document.querySelector('.orderchange-btn')
+export const spinner = document.querySelector('.spinner-border')
+export const select = document.querySelector('.form-select')
+
+export const state = {
+  editlist: null,
+  editupdate: null, 
+  editID: null, 
+  editOrder: null, 
+  editDone: null, 
+  update: null, 
+  editFlag: false,
+  overlap: false,
+  idArray: [],
+}
+
